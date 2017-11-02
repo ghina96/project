e70c6215c2bd827b727544a3b1dcd4173f73d6f1
ghina96 <32621015+ghina96@users.noreply.github.com>
2017-11-02 19:34:52 +0100

    Update examples.js

--- sys/examples.js
+++ sys/examples.js
@@ -21,16 +21,11 @@
 'use strict';
 
 var HyperSwitch = require('hyperswitch');
+const URI = HyperSwitch.URI;
 var path = require('path');
-
 var fsUtil = require('../lib/FeatureServiceUtil');
 
 var spec = HyperSwitch.utils.loadSpec(path.join(__dirname, 'examples.yaml'));
-
-// EXampleService
-function EXS(options) {
-    this.options = options;
-}
 
 const STEP_TO_SECONDS = {
     second:    1,
@@ -39,44 +34,120 @@ const STEP_TO_SECONDS = {
     day:   86400
 };
 
-/*
- * Function called from endpoint to generate a fake timeserie
- */
-EXS.prototype.fakeTimeserie = function(hyper, req) {
-    var requestParams = req.params;
 
-    fsUtil.validateFromAndTo(requestParams);
 
-    var fromDate = requestParams.fromDate;
-    var intervalSeconds = (requestParams.toDate - fromDate) / 1000;
-    var stepSeconds = STEP_TO_SECONDS[requestParams.step];
+class TimeserieTreament {
+    constructor(options) {
+        this.options = options;
+	}
 
-    if (stepSeconds > intervalSeconds) {
-        fsUtil.throwIfNeeded('Step should be smaller than [from, to[ interval');
-    }
+	fakeTimeserie(hyper, req) {
+	    var requestParams = req.params;
 
-    var stepNumbers = intervalSeconds / stepSeconds;
+	    fsUtil.validateFromAndTo(requestParams);
 
-    return fsUtil.normalizeResponse({
-        status: 200,
-        body: {
-            items: [...Array(stepNumbers).keys()].map(idx => {
-                return {
-                    ts: (new Date(fromDate.getTime() + (idx * stepSeconds * 1000))).toISOString(),
-                    val: Math.random()
-                };
-            })
-        }
-    });
-};
+	    var fromDate = requestParams.fromDate;
+	    var intervalSeconds = (requestParams.toDate - fromDate) / 1000;
+	    var stepSeconds = STEP_TO_SECONDS[requestParams.step];
+
+	    if (stepSeconds > intervalSeconds) {
+	        fsUtil.throwIfNeeded('Step should be smaller than [from, to[ interval');
+	    }
+
+	    var stepNumbers = intervalSeconds / stepSeconds;
+
+	    return fsUtil.normalizeResponse({
+	        status: 200,
+	        body: {
+	            items: [...Array(stepNumbers).keys()].map(idx => {
+	                return {
+	                    ts: (new Date(fromDate.getTime() + (idx * stepSeconds * 1000))).toISOString(),
+	                    val: Math.random()
+	                };
+	            })
+	        }
+	    });
+	}
+
+	async meanTimeserie(hyper, req) {
+		var requestParams = req.params;
+	    fsUtil.validateFromAndTo(requestParams);
+
+		const uriFakeTS = new URI([requestParams.domain, 'sys', 'examples',
+			'fake-timeserie',requestParams.from,
+			requestParams.to,requestParams.step]);
+
+		var avg = await hyper.get({ uri: uriFakeTS }).then((res) =>
+			res.body.items.map(items => items.val).
+				reduce((prev, next) => prev + next, 0)/res.body.items.length);
+
+	    return fsUtil.normalizeResponse({
+	        status: 200,
+	        body: {
+	            items:
+					avg
+			}
+	    });
+
+	};
+// contribution of ghina abdallah
+	
+	async minTimeserie(hyper, req) {
+		var requestParams = req.params;
+	    fsUtil.validateFromAndTo(requestParams);
+
+		const uriFakeTS = new URI([requestParams.domain, 'sys', 'examples',
+			'fake-timeserie',requestParams.from,
+			requestParams.to,requestParams.step]);
+
+		var minimum = await hyper.get({ uri: uriFakeTS }).then((res) =>
+			res.body.items.map(items => items.val).
+				reduce((prev, next) => {return Math.min(prev, next);
+		}));
+				
+	    return fsUtil.normalizeResponse({
+	        status: 200,
+	        body: {
+	            items:
+					minimum
+			}
+	    });
+
+	};
+
+	async maxTimeserie(hyper, req) {
+		var requestParams = req.params;
+	    fsUtil.validateFromAndTo(requestParams);
+
+		const uriFakeTS = new URI([requestParams.domain, 'sys', 'examples',
+			'fake-timeserie',requestParams.from,
+			requestParams.to,requestParams.step]);
+
+		var maximum = await hyper.get({ uri: uriFakeTS }).then((res) =>
+			res.body.items.map(items => items.val).
+				reduce((prev, next) => {return Math.max(prev, next);
+		}));
+				
+	    return fsUtil.normalizeResponse({
+	        status: 200,
+	        body: {
+	            items:
+					maximum
+			}
+	    });
+
+	};
+	// to here ***********/	
+}
 
 module.exports = function(options) {
-    var exs = new EXS(options);
+	var tst = new TimeserieTreament(options);
 
     return {
         spec: spec,
         operations: {
-            fakeTimeserie: exs.fakeTimeserie.bind(exs)
+            fakeTimeserie: tst.fakeTimeserie.bind(tst),
+			meanTimeserie: tst.meanTimeserie.bind(tst)
         }
     };
 };
